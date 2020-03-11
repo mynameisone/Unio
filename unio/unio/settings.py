bf5d4ca15a6d07259172e7f1e105d0945e1a9c72
Azimjon Kamolov <azimjon.6561@gmail.com>
2020-03-11 19:33:20 +0900

    accounts.urls problem popped up

--- unio/unio/settings.py
+++ unio/unio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'accounts',
 ]
 
@@ -125,3 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES = [os.path.join(BASE_DIR, 'static')]
+
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'test'
