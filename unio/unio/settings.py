f1949dbe802b73676c5cf06460ef27b5c35127e7
Azimjon Kamolov <azimjon.6561@gmail.com>
2020-03-12 18:06:47 +0900

    newer version is a must

--- unio/unio/settings.py
+++ unio/unio/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
-    'accounts',
 ]
 
 MIDDLEWARE = [
